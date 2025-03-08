33c4632ba5b80a2321e1ed45b8a9cafe00c66bb6
larissa <larza.ault@gmail.com>
2025-03-08 12:34:11 -0800

    added filtering for adventures and blogs get endpoints when query params provided

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,13 @@ blog_bp = Blueprint("blog", __name__, url_prefix="/blogs")
 # adventures
 @adventures_bp.route("", methods=["GET"])
 def get_adventures():
-    adventures = Adventure.query.order_by(Adventure.id).all()
+    name_query = request.args.get("name")
+
+    if name_query:
+        adventures = Adventure.query.filter_by(alt_name=name_query)
+    else:
+        adventures = Adventure.query.order_by(Adventure.id).all()
+
     response = []
     for adv in adventures:
         response.append(adv.to_dict())
@@ -19,10 +25,16 @@ def get_adventures():
 # blog and contents
 @blog_bp.route("", methods=["GET"])
 def get_blog_content():
-    id_ = int(request.headers.get('Adventure-ID'))
-    blog = Blog.query.filter_by(adventure_id=id_).first()
+    name_query = request.args.get("name")
+    
+    if name_query:
+        blogs = Blog.query.filter_by(alt_name=name_query)
+    else:
+        blogs = Blog.query.order_by(Blog.id).all()
 
-    response = blog.to_dict()
-    for content in blog.contents:
-        response['content'].append(content.to_dict())
+    response = []
+    for i, blog in enumerate(blogs):
+        response.append(blog.to_dict())
+        for content in blog.contents:
+            response[i]['content'].append(content.to_dict())
     return jsonify(response)
